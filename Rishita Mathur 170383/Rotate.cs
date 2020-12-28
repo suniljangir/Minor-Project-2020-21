372580bf7001e1305593d96472e6eb76676de8f5
Rishita Mathur <38039850+rishitamathur27@users.noreply.github.com>
2020-12-28 16:46:35 +0530

    Minor Files

---
+++ Rishita Mathur 170383/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public int Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector4 (0,Time.deltaTime*Speed,0));
+    }
+}
